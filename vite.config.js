d07f5fef8ce27581744d80ba9a7ecad34e65fdba
Langton49 <munashemukweya2022@gmail.com>
2025-06-23 02:21:23 -0400

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem'))
+    },
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin'
+    }
+  }
+})
